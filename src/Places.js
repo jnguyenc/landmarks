e549268e961f6faf296196ca8b639ebe5d158b33
John Nguyen <jnguyenc@gmail.com>
2022-02-12 23:56:21 -0500

    refactored, cleaned up Places.js

--- src/Places.js
+++ src/Places.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import './Places.scss';
-//import imagesData from './imgs/imgs.json';
-// Why is import is not working?
+import jsonData from './imgs/images.json';
+//const jsonData = require('./imgs/images.json'); // use either import or require
+
+const places = jsonData.places;
 
 export default function ImageBoard() {
-    const data = [
-        { path: 'pexels-aleksandar-pasaric-325185.jpg' },
-        { path: 'pexels-asad-photo-maldives-3293148.jpg' },
-        { path: 'pexels-errin-casano-2356045.jpg' },
-        { path: 'pexels-francesco-ungaro-2835436.jpg' },
-        { path: 'pexels-irina-iriser-1408221.jpg' },
-        { path: 'pexels-luis-del-río-15286.jpg' },
-        { path: 'pexels-maxime-francis-2246476.jpg' },
-    ];
-
-    const elements = data.map((e) => {
-        // let i = `./imgs/${e.path}`;
-        // return <img alt="" className="image" src={i} />;
-        return <img alt="" className="image" src={`./imgs/${e.path}`} />;
+    const elements = places.map((e) => {
+        return <img alt="" key={e.path} className="image" src={`./imgs/${e.path}`} />;
     });
 
     return (
